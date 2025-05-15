a8a371331c2544d596a027392b45284aea71ee72
voinov <voinov@skbkontur.ru>
2025-05-15 21:50:26 +0500

    fix order sort

--- app/services/exchange_service.py
+++ app/services/exchange_service.py
@@ -31,10 +31,10 @@ class ExchangeService:
         bids = [order for order in active_orders if order.direction == Direction.BUY and order.type == 'limit']
         asks = [order for order in active_orders if order.direction == Direction.SELL and order.type == 'limit']
 
-        # Сортируем bids по убыванию цены (лучшая цена покупки - самая высокая)
-        bids.sort(key=lambda x: x.price, reverse=True)
-        # Сортируем asks по возрастанию цены (лучшая цена продажи - самая низкая)
-        asks.sort(key=lambda x: x.price)
+        # Сортируем bids по возрастанию цены (лучшая цена покупки - самая низкая)
+        bids.sort(key=lambda x: x.price)
+        # Сортируем asks по убыванию цены (лучшая цена продажи - самая высокая)
+        asks.sort(key=lambda x: x.price, reverse=True)
 
         # Группируем по ценовым уровням и суммируем количество
         bid_levels = {}
